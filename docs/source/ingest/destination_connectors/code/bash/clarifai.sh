4a7387201a3d1fa7047121e4a4503528f90a6730
mogith-pn <143642606+mogith-pn@users.noreply.github.com>
2024-02-19 09:44:23 +0000

    Clarifai- Added examples in docs

--- docs/source/ingest/destination_connectors/code/bash/clarifai.sh
+++ docs/source/ingest/destination_connectors/code/bash/clarifai.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-# Uploads the structured output of the files within the given path to a clarifai app.
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-cd "$SCRIPT_DIR"/../../.. || exit 1
-
-PYTHONPATH=. ./unstructured/ingest/main.py \
+unstructured-ingest \
   local \
   --input-path example-docs/book-war-and-peace-1225p.txt \
   --output-dir local-output-to-clarifai \
